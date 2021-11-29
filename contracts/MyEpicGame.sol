44c01c3b1cc9f21e4747e0607227c38134467566
vinayakpahalwan7 <pahalwan.vinayak@protonmail.com>
2021-11-29 18:41:23 +0530

    setup data for NFTs

--- contracts/MyEpicGame.sol
+++ contracts/MyEpicGame.sol
@@ -5,7 +5,39 @@ pragma solidity ^0.8.0;
 import "hardhat/console.sol";
 
 contract MyEpicGame {
-    constructor() {
-        console.log("This is my first contract");
+    //Character attributes
+    struct CharacterAttributes {
+        uint characterIndex;
+        string name;
+        string vidURI;
+        uint hp;
+        uint maxHp;
+        uint attackDamage;
+    }
+    //Default data for all characters.
+    CharacterAttributes[] defaultCharacters;
+
+    //Pass values into the contract from run.js
+    constructor(
+        string[] memory characterNames,
+        string[] memory characterVidURIs,
+        uint[] memory characterHp,
+        uint[] memory characterAttackDmg
+    )
+    {
+        //loop through all characters, to save their values in our contract.
+        for(uint i = 0; i<characterNames.length; i+=1) {
+            defaultCharacters.push(CharacterAttributes({
+                characterIndex: i,
+                name: characterNames[i],
+                vidURI: characterVidURIs[i],
+                hp: characterHp[i],
+                maxHp: characterHp[i],
+                attackDamage: characterAttackDmg[i]
+            }));
+
+            CharacterAttributes memory c = defaultCharacters[i];
+            console.log("Done initializing %s w/ HP %s, img %s", c.name, c.hp, c.vidURI);
+        }
     }
 }
