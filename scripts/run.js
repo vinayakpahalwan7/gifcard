ee588f7c9aff27c5fff4a695ccfc53ac2741318d
vinayakpahalwan7 <pahalwan.vinayak@protonmail.com>
2021-11-29 19:01:30 +0530

    add defaults

--- scripts/run.js
+++ scripts/run.js
@@ -4,7 +4,18 @@ const main = async() => {
     //This will compile the contract and generate necessary fules we need to work our contracts under the artifacts directory.
     //Hardhat will create a local eth network, after script completes - it is destroyed. Hardhat also create fake miners on the machine to imitate actual blockchain.
     const gameContractFactory = await hre.ethers.getContractFactory('MyEpicGame');
-    const gameContract = await gameContractFactory.deploy();
+    const gameContract = await gameContractFactory.deploy(
+        ["Dave", "John", "Coach", "Kobe", "Ronaldo", "Lebron", "Steph"],
+        ["https://i.imgur.com/zRKUVBS.mp4",
+        "https://i.imgur.com/17djyaF.mp4",
+        "https://i.imgur.com/frnXxyM.mp4",
+        "https://i.imgur.com/Ms7PR7F.mp4",
+        "https://i.imgur.com/1kSZ05R.mp4",
+        "https://i.imgur.com/pOHFkGP.mp4",
+        "https://i.imgur.com/kdOE35B.mp4"],
+        [1000, 400, 200, 500, 400, 600, 500],
+        [100, 100, 100, 100, 100, 100, 100]
+    );
     await gameContract.deployed();
     console.log("Contract deployed to: ", gameContract.address);
 };
